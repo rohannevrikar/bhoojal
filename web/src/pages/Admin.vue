7f1da0305b0b60138fc63dc2339088c8fedbbb4d
Om <omkarkhair@gmail.com>
2021-07-04 16:26:40 +0530

    basic axios setup

--- web/src/pages/Admin.vue
+++ web/src/pages/Admin.vue
@@ -6,8 +6,16 @@
           <h4 class="card-title">Admin Dashboard</h4>
           <p class="card-category">
             This dashboard is accessible for the public in interest of
-            transparency. Select a region and view localized ground water health ratings. Ratings are provided based on ground water consumption patterns combined with water percolation metrics.
+            transparency. Select a region and view localized ground water health
+            ratings. Ratings are provided based on ground water consumption
+            patterns combined with water percolation metrics.
           </p>
+          <p>
+            <button v-on:click="loadHeatmap">Load Heatmap</button>
+          </p>
+          <div v-for="(p, i) in heatmap" :key="i + 10">
+            <span>{{p}}</span>
+          </div>
         </template>
         <AzureMap :center="getfilterCityLocation" :zoom="12" height="600px">
           <AzureMapDataSource
@@ -31,7 +39,7 @@ import {
   AzureMapHeatMapLayer,
 } from "vue-azure-maps";
 
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   components: {
@@ -40,11 +48,46 @@ export default {
     AzureMapPoint,
     AzureMapHeatMapLayer,
   },
-  methods: {},
-  mounted () {
+  data() {
+    return {
+      cities: [
+        {
+          name: "Pune",
+          id: "pune",
+        },
+        {
+          name: "PCMC",
+          id: "pcmc",
+        },
+        {
+          name: "Mumbai",
+          id: "mumbai",
+        },
+        {
+          name: "Thane",
+          id: "thane",
+        },
+      ],
+      selectedCityId: "pune",
+      heatmap: []
+    };
+  },
+  methods: {
+    loadHeatmap: function () {
+      console.log("Loading heatmap for city");
+      let scope = this;
+      axios
+        .get("https://bhoojal-data-cdn.azureedge.net/heatmaps/pune/2020Q3.json")
+        .then((response) => {
+          console.log(JSON.parse(response.data.result));
+          scope.heatmap = JSON.parse(response.data.result);
+        });
+    },
+  },
+  mounted() {
     axios
-      .get('https://bhoojal-data-cdn.azureedge.net/heatmaps/pune/2020Q3.json')
-      .then(response => { console.log("coinss") })
-  }
+      .get("https://bhoojal-data-cdn.azureedge.net/heatmaps/pune/2020Q3.json")
+      .then((response) => {});
+  },
 };
 </script>
